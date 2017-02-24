0f78f59db95679ed260966de9ab1ae1b26f3c5aa
Bruno da Luz <brunolabx@gmail.com>
2017-02-24 09:51:42 -0300

    deleted storyboard

--- Couch/Couch/ViewController.swift
+++ Couch/Couch/ViewController.swift
@@ -1,25 +1,16 @@
-//
-//  ViewController.swift
-//  Couch
-//
-//  Created by Bruno da Luz on 2/24/17.
-//  Copyright © 2017 Bruno da Luz. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.view.backgroundColor = .white
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
 
 }
 
