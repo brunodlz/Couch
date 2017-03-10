1bac6131bf56b01e9b175c48f3975a1cab63fd19
Bruno da Luz <brunolabx@gmail.com>
2017-03-09 23:31:31 -0300

    adding blur effect

--- Couch/Couch/Extension/ImageView+Extension.swift
+++ Couch/Couch/Extension/ImageView+Extension.swift
@@ -5,4 +5,13 @@ extension UIImageView {
         self.image = self.image!.withRenderingMode(.alwaysTemplate)
         self.tintColor = color
     }
+    
+    func addBlur() {
+        let blurEffect = UIBlurEffect(style: .dark)
+        let blurEffectView = UIVisualEffectView(effect: blurEffect)
+        blurEffectView.frame = self.bounds
+        
+        blurEffectView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        self.addSubview(blurEffectView)
+    }
 }
