7d22d457966f16ce01dc1117ab64f418afba5eab
Bruno da Luz <brunolabx@gmail.com>
2017-03-11 23:36:49 -0300

    adding container view

--- Couch/Couch/View/Show/Profile/ProfileView.swift
+++ Couch/Couch/View/Show/Profile/ProfileView.swift
@@ -13,6 +13,15 @@ final class ProfileView: UIView {
         locationLabel.text = profile.location
         ageLabel.text = "\(profile.age!) years old"
     }
+
+    let containerView: UIView = {
+        let view = UIView(frame: .zero)
+        view.backgroundColor = .clear
+        view.layer.cornerRadius = 8
+        view.layer.borderColor = ColorPalette.white.cgColor
+        view.layer.borderWidth = 1.0
+        return view
+    }()
     
     let usernameLabel: UILabel = {
         let label = UILabel()
@@ -60,52 +69,61 @@ final class ProfileView: UIView {
 extension ProfileView: ViewConfiguration {
     func setupConstraints() {
         
-        usernameLabel.snp.makeConstraints { (make) in
-            make.top.equalTo(self).offset(20)
-            make.left.equalTo(self)
-            make.right.equalTo(self)
-            make.height.equalTo(22)
+        containerView.snp.makeConstraints { (make) in
+            make.top.equalTo(self).offset(8)
+            make.left.equalTo(self).offset(8)
+            make.right.equalTo(self).offset(-8)
+            make.bottom.equalTo(self).offset(-8)
         }
-        
-        avatarImage.snp.makeConstraints { (make) in
-            make.top.equalTo(usernameLabel.snp.bottom).offset(8)
-            make.centerX.equalTo(self)
-            make.width.equalTo(120)
-            make.height.equalTo(120)
-        }
-        
-        memberSinceLabel.snp.makeConstraints { (make) in
-            make.top.equalTo(avatarImage.snp.bottom).offset(8)
-            make.left.equalTo(self).offset(32)
-            make.right.equalTo(self).offset(-32)
-        }
-        
-        aboutLabel.snp.makeConstraints { (make) in
-            make.top.equalTo(memberSinceLabel.snp.bottom).offset(8)
-            make.left.equalTo(self).offset(32)
-            make.right.equalTo(self).offset(-32)
-        }
-        
-        locationLabel.snp.makeConstraints { (make) in
-            make.top.equalTo(aboutLabel.snp.bottom).offset(8)
-            make.left.equalTo(self).offset(32)
-            make.right.equalTo(self).offset(-32)
-        }
-        
-        ageLabel.snp.makeConstraints { (make) in
-            make.top.equalTo(locationLabel.snp.bottom).offset(8)
-            make.left.equalTo(self).offset(32)
-            make.right.equalTo(self).offset(-32)
-        }
+//        
+//        usernameLabel.snp.makeConstraints { (make) in
+//            make.top.equalTo(self).offset(20)
+//            make.left.equalTo(self)
+//            make.right.equalTo(self)
+//            make.height.equalTo(22)
+//        }
+//        
+//        avatarImage.snp.makeConstraints { (make) in
+//            make.top.equalTo(usernameLabel.snp.bottom).offset(8)
+//            make.centerX.equalTo(self)
+//            make.width.equalTo(120)
+//            make.height.equalTo(120)
+//        }
+//        
+//        memberSinceLabel.snp.makeConstraints { (make) in
+//            make.top.equalTo(avatarImage.snp.bottom).offset(8)
+//            make.left.equalTo(self).offset(32)
+//            make.right.equalTo(self).offset(-32)
+//        }
+//        
+//        aboutLabel.snp.makeConstraints { (make) in
+//            make.top.equalTo(memberSinceLabel.snp.bottom).offset(8)
+//            make.left.equalTo(self).offset(32)
+//            make.right.equalTo(self).offset(-32)
+//        }
+//        
+//        locationLabel.snp.makeConstraints { (make) in
+//            make.top.equalTo(aboutLabel.snp.bottom).offset(8)
+//            make.left.equalTo(self).offset(32)
+//            make.right.equalTo(self).offset(-32)
+//        }
+//        
+//        ageLabel.snp.makeConstraints { (make) in
+//            make.top.equalTo(locationLabel.snp.bottom).offset(8)
+//            make.left.equalTo(self).offset(32)
+//            make.right.equalTo(self).offset(-32)
+//        }
     }
     
     func buildViewHierarchy() {
-        self.addSubview(usernameLabel)
-        self.addSubview(avatarImage)
-        self.addSubview(memberSinceLabel)
-        self.addSubview(aboutLabel)
-        self.addSubview(locationLabel)
-        self.addSubview(ageLabel)
+//        self.addSubview(usernameLabel)
+//        self.addSubview(avatarImage)
+//        self.addSubview(memberSinceLabel)
+//        self.addSubview(aboutLabel)
+//        self.addSubview(locationLabel)
+//        self.addSubview(ageLabel)
+        
+        self.addSubview(containerView)
     }
     
     func configureViews() {
